7182b0b584147dcb10fc586a77a1b198f20d848a
Sufian Zaabalawi <s.zaabalawi@albellus.com>
2020-08-06 19:23:28 +0200

    data augmentation and preparation

--- train.py
+++ train.py
@@ -1,33 +1,34 @@
 import os
-import random
 import pandas as pd
 import numpy as np
 from glob import glob
 from PIL import Image
-import tensorflow as tf
-from keras_preprocessing.image import ImageDataGenerator
-import matplotlib.pyplot as plt
+from utils.augmentation import save_augmentation
 from skimage import exposure
 from data_analysis import clean_data
+import pickle
 
 
 class DataPreparation:
-    def __init__(self, csv_path, images_path):
+    def __init__(self, csv_path, src_dir, dst_dir):
         self.IMAGE_SIZE = (384, 384)
         self.CLASS_TARGETS = ['No Finding', ' Other', 'COVID-19']
         self.CLASS_NUM = len(self.CLASS_TARGETS)
-        self.images_list = glob(images_path, recursive=True)
-        self.df = clean_data(pd.read_csv(csv_path))
+        self.images_list = glob(os.path.join(src_dir, '*'), recursive=True)
+        self.df = pd.read_csv(csv_path)
+        self.dst_dir = dst_dir
+        if not os.path.exists(self.dst_dir):
+            os.makedirs(self.dst_dir)
 
     def load(self):
-        data = {'images': [], 'features': [], 'labels': []}
+        data = {'paths': [], 'features': [], 'labels': []}
+        num_aug_images = 0
         for image_path in self.images_list:
             image_name = os.path.basename(image_path)
             # extract features from csv
             csv_row = self.df.loc[self.df.File == image_name]
-            if csv_row.empty: continue
+            features = np.array([*csv_row['Age'].values, csv_row['Sex'].values])
 
-            features = np.array([*csv_row['Age'].values, 0 if csv_row['Sex'].values == 'Female' else 1])
             image = np.array(Image.open(image_path).resize(self.IMAGE_SIZE))
 
             # convert RGB to grayscale
@@ -52,18 +53,26 @@ class DataPreparation:
                 label[1] = 1
             if no_finding:
                 label[0] = 1
-
-            # throw error if label not vaild
+                # throw error if label not vaild
             if np.count_nonzero(label) != 1:
                 raise ValueError('CSV Data Error: Found no corresponding label in row {}'.format(csv_row))
 
+            if covid:
+                save_augmentation(image, image_name, label, features, data, self.dst_dir)
+
+            if no_finding and num_aug_images < 600:
+                save_augmentation(image, image_name, label, features, data, self.dst_dir, aug_num=1)
+                num_aug_images += 1
+
             # add data to batch
-            data['images'].append(image)
+            data['paths'].append(image_name)
             data['features'].append(features)
             data['labels'].append(label)
-
-        return np.array(data['images']), np.array(data['features']), np.array(data['labels'])
+            result = Image.fromarray((image * 255).astype(np.uint8))
+            result.save(os.path.join(self.dst_dir, image_name))
+        with open('paths_features_labels.pkl', 'wb') as f:
+            pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
 
 
 if __name__ == '__main__':
-    X, f, y = DataPreparation('metadata.csv', 'images/*').load()
+    DataPreparation(csv_path='metadata.csv', src_dir='images', dst_dir='train_data').load()
---
+++ utils/augmentation.py
@@ -0,0 +1,120 @@
+import random
+import os
+from skimage.transform import AffineTransform, warp
+import numpy as np
+from PIL import Image
+from skimage.util import random_noise
+
+
+def flip(image):
+    return np.fliplr(image)
+
+
+def noise_bright(image):
+    res = image + 0.15
+    res[res > 1] = 1
+    return random_noise(res, var=0.01)
+
+
+def bright(image):
+    res = image + 0.15
+    res[res > 1] = 1
+    return res
+
+
+def shear(image):
+    r = random.uniform(-0.15, 0.15)
+    res = warp(image, AffineTransform(shear=r), order=1, preserve_range=True, mode='wrap')
+    return res
+
+
+def dark(image):
+    res = image - 0.15
+    res[res < 0] = 0
+    return res
+
+
+def noise_bright_shear(image):
+    r = random.uniform(-0.15, 0.15)
+    res = image + 0.15
+    res[res > 1] = 1
+    res = warp(random_noise(res, var=0.01), AffineTransform(shear=r), order=1, preserve_range=True, mode='wrap')
+    return res
+
+
+def flip_shear_noise(image):
+    r = random.uniform(-0.15, 0.15)
+    res = warp(np.fliplr(random_noise(image, var=0.01)), AffineTransform(shear=r), order=1, preserve_range=True,
+               mode='wrap')
+    return res
+
+
+def flip_dark(image):
+    res = image - 0.15
+    res[res < 0] = 0
+    res = np.fliplr(res)
+    return res
+
+
+def noise(image):
+    return random_noise(image, var=0.01)
+
+
+def shear_noise_dark(image):
+    r = random.uniform(-0.15, 0.15)
+    res = image - 0.15
+    res[res < 0] = 0
+    res = warp(random_noise(res, var=0.01), AffineTransform(shear=r), order=1, preserve_range=True, mode='wrap')
+    return res
+
+
+def flip_shear(image):
+    r = random.uniform(-0.15, 0.15)
+    res = warp(np.fliplr(image), AffineTransform(shear=r), order=1, preserve_range=True, mode='wrap')
+    return res
+
+
+def flip_noise(image):
+    return np.fliplr(random_noise(image, var=0.01))
+
+
+STRATEGY_PICKER = {
+    'flip': flip,
+    'noise_bright': noise_bright,
+    'bright': bright,
+    'shear': shear,
+    'dark': dark,
+    'noise_bright_shear': noise_bright_shear,
+    'flip_shear_noise': flip_shear_noise,
+    'flip_dark': flip_dark,
+    'noise': noise,
+    'shear_noise_dark': shear_noise_dark,
+    'flip_shear': flip_shear,
+    'flip_noise': flip_noise
+}
+STRATEGIES = [
+    'flip',
+    'noise_bright',
+    'bright',
+    'shear',
+    'dark',
+    'noise_bright_shear',
+    'flip_shear_noise',
+    'flip_dark',
+    'noise',
+    'shear_noise_dark',
+    'flip_shear',
+    'flip_noise'
+]
+
+
+def save_augmentation(image, image_name, label, features, batch, dst_dir, aug_num=12):
+    random.shuffle(STRATEGIES)
+    strategy_arr = STRATEGIES[:aug_num]
+    for strategy in strategy_arr:
+        res = STRATEGY_PICKER[strategy](image)
+        result = Image.fromarray((res * 255).astype(np.uint8))
+        result.save(os.path.join(dst_dir, 'Aug' + strategy + '_' + image_name))
+        batch['paths'].append('Aug' + strategy + '_' + image_name)
+        batch['features'].append(features)
+        batch['labels'].append(label)
