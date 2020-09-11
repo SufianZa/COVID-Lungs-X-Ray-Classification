a4302cf6c08b4cc397082dcd655944b93220fcce
Sufian Zaabalawi <s.zaabalawi@albellus.com>
2020-09-11 11:09:50 +0200

    more augmenation

--- utils/augmentation.py
+++ utils/augmentation.py
@@ -1,6 +1,6 @@
 import random
 import os
-from skimage.transform import AffineTransform, warp
+from skimage.transform import AffineTransform, warp, rotate
 import numpy as np
 from PIL import Image
 from skimage.util import random_noise
@@ -10,20 +10,19 @@ def flip(image):
     return np.fliplr(image)
 
 
-def noise_bright(image):
-    res = image + 0.15
-    res[res > 1] = 1
-    return random_noise(res, var=0.01)
-
-
 def bright(image):
     res = image + 0.15
     res[res > 1] = 1
     return res
 
 
+def noise(image):
+    return random_noise(image, var=0.01)
+
+
 def shear(image):
-    r = random.uniform(-0.15, 0.15)
+    flag = bool(random.getrandbits(1))
+    r = -0.2 if flag else 0.2
     res = warp(image, AffineTransform(shear=r), order=1, preserve_range=True, mode='wrap')
     return res
 
@@ -34,63 +33,105 @@ def dark(image):
     return res
 
 
+def rotation(image):
+    flag = bool(random.getrandbits(1))
+    r = random.randint(-70, -50) if flag else random.randint(50, 70)
+    return rotate(image, angle=r)
+
+
+def noise_bright(image):
+    return noise(bright(image))
+
+
 def noise_bright_shear(image):
-    r = random.uniform(-0.15, 0.15)
-    res = image + 0.15
-    res[res > 1] = 1
-    res = warp(random_noise(res, var=0.01), AffineTransform(shear=r), order=1, preserve_range=True, mode='wrap')
-    return res
+    return noise_bright(shear(image))
 
 
 def flip_shear_noise(image):
-    r = random.uniform(-0.15, 0.15)
-    res = warp(np.fliplr(random_noise(image, var=0.01)), AffineTransform(shear=r), order=1, preserve_range=True,
-               mode='wrap')
-    return res
+    return flip(shear(noise(image)))
 
 
 def flip_dark(image):
-    res = image - 0.15
-    res[res < 0] = 0
-    res = np.fliplr(res)
-    return res
-
-
-def noise(image):
-    return random_noise(image, var=0.01)
+    return flip(dark(image))
 
 
 def shear_noise_dark(image):
-    r = random.uniform(-0.15, 0.15)
-    res = image - 0.15
-    res[res < 0] = 0
-    res = warp(random_noise(res, var=0.01), AffineTransform(shear=r), order=1, preserve_range=True, mode='wrap')
-    return res
+    return shear(noise(dark(image)))
 
 
 def flip_shear(image):
-    r = random.uniform(-0.15, 0.15)
-    res = warp(np.fliplr(image), AffineTransform(shear=r), order=1, preserve_range=True, mode='wrap')
-    return res
+    return flip(shear(image))
 
 
 def flip_noise(image):
-    return np.fliplr(random_noise(image, var=0.01))
+    return flip(noise(image))
+
+
+def flip_rotation(image):
+    return flip(rotation(image))
+
+
+def noise_rotation(image):
+    return noise(rotation(image))
+
+
+def shear_rotation_dark(image):
+    return shear(rotation(dark(image)))
+
+
+def flip_shear_rotation(image):
+    return flip(shear(rotation(image)))
+
+
+def rotation_bright(image):
+    return rotation(bright(image))
+
+
+def rotation_bright_shear(image):
+    return rotation(bright(shear(image)))
+
+
+def flip_rotation_bright_shear(image):
+    return rotation_bright_shear(flip(image))
+
+
+def flip_dark_noise(image):
+    return flip_dark(noise(image))
+
+
+def shear_noise(image):
+    return shear(noise(image))
+
+
+def shear_noise(image):
+    return shear(rotation(image))
+
+
+def rotation_shear(image):
+    return rotation(shear(image))
 
 
 STRATEGY_PICKER = {
     'flip': flip,
-    'noise_bright': noise_bright,
+    'rotation_bright': rotation_bright,
     'bright': bright,
     'shear': shear,
     'dark': dark,
-    'noise_bright_shear': noise_bright_shear,
-    'flip_shear_noise': flip_shear_noise,
+    'rotation_bright_shear': rotation_bright_shear,
+    'flip_shear_rotation': flip_shear_rotation,
     'flip_dark': flip_dark,
+    'flip_shear_noise': flip_shear_noise,
     'noise': noise,
-    'shear_noise_dark': shear_noise_dark,
+    'shear_rotation_dark': shear_rotation_dark,
     'flip_shear': flip_shear,
-    'flip_noise': flip_noise
+    'flip_rotation': flip_rotation,
+    'rotation': rotation,
+    'noise_bright_shear': noise_bright_shear,
+    'noise_rotation': noise_rotation,
+    'flip_rotation_bright_shear': flip_rotation_bright_shear,
+    'flip_dark_noise': flip_dark_noise,
+    'shear_noise': shear_noise,
+    'rotation_shear': rotation_shear
 }
 
 
